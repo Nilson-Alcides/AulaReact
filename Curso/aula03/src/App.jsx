7ff637d6a8f30544f723b566a63ac44af87228b8
Nilson-Alcides <nilson_alcides@hotmail.com>
2024-07-31 15:46:05 -0300

    Add primeiro componente

---
+++ Curso/aula03/src/App.jsx
@@ -0,0 +1,18 @@
+// Importando css
+import './App.css'
+// Importando Componente
+import PrimeiroComponente from './Componente/PrimeiroComponente'
+
+function App() {
+  return (
+    <>
+      <h1>Pagina inicial </h1>
+      {/*Comentario dentro da função*/}
+      
+      {/*Add componente*/}
+      <PrimeiroComponente/>
+    </>
+  )
+}
+
+export default App
---
+++ Curso/aula03/src/Componente/PrimeiroComponente.jsx
@@ -0,0 +1,10 @@
+const PrimeiroComponente = () =>{
+    return(
+        <div>
+            <p>
+                Meu primeiro componente.
+            </p>
+        </div>
+    )
+}
+export default PrimeiroComponente
